8854cde00d7365e1ede77638b5457c9bdabf001f
Fran Jurinec <franjurinec@live.com>
2023-02-06 01:40:44 +0200

    Added Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { Box, Container, Flex, Heading, Text } from '@chakra-ui/react'
+
 function App() {
-  return <div>Hello Cloudflare!</div>
+  return (
+    <Box>
+      <Flex
+        alignItems={'center'}
+        as={'header'}
+        px={'16'}
+        height={'32'}
+        mb={'16'}
+        borderBottom={'2px'}
+      >
+        <Heading size={'3xl'} stroke={''}>
+          GOOD.
+          <Text as={'span'} color={'blackAlpha.400'}>
+            STORE
+          </Text>
+        </Heading>
+      </Flex>
+      <Container maxWidth={'8xl'}>Hello Cloudflare!</Container>
+    </Box>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Inter', sans-serif`,
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
