5911f8db0d89eec91428ac0ebeae30c6560b0eb8
Fran Jurinec <franjurinec@live.com>
2023-02-22 22:32:43 +0200

    Added react-query

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import theme from './theme'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Admin from './components/Admin'
 import Store from './components/Store'
+import theme from './theme'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Store />} />
-          <Route path="admin/*" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Store />} />
+            <Route path="admin/*" element={<Admin />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
