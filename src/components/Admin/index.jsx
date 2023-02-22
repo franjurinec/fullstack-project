09f33a477d37b32bfe9f118bccdd81ce37581172
Fran Jurinec <franjurinec@live.com>
2023-02-23 01:20:19 +0200

    Lint Fix

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,4 +1,3 @@
-import { Route, Routes } from 'react-router-dom'
 import AdminLogin from './AdminLogin'
 import AdminControlPanel from './AdminControlPanel'
 import { isAuthenticated } from '../../services/authService'
