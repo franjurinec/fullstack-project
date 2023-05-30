f3864a29ad065f8fba9a547df4c4b277c7734678
Fran Jurinec <franjurinec@live.com>
2023-05-30 20:25:11 +0300

    Admin Product Table UI Change

--- src/components/Admin/AdminControlPanel/ManageProducts/index.jsx
+++ src/components/Admin/AdminControlPanel/ManageProducts/index.jsx
@@ -1,9 +1,13 @@
-import { Box } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
+import Button from '../../../common/Button'
 import AdminProductsTable from './AdminProductsTable'
 
 const ManageProducts = () => (
-  <Box>
+  <Flex flexDir={'column'} flexGrow={1}>
+    <Button mb={8} w={48}>
+      + New Product
+    </Button>
     <AdminProductsTable />
-  </Box>
+  </Flex>
 )
 export default ManageProducts
