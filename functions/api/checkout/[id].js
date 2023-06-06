8006c5e6f4d612eb0dd874b81a293b3d8cf8ff8b
Fran Jurinec <franjurinec@live.com>
2023-06-07 02:37:32 +0300

    Removed Unused Code

--- functions/api/checkout/[id].js
+++ functions/api/checkout/[id].js
@@ -1,17 +1,5 @@
 import Stripe from 'stripe'
 
-// const simpleSession = (product) => ({
-//   id: product.id,
-//   name: product.name,
-//   description: product.description,
-//   image: product.images[0],
-//   price: new Intl.NumberFormat('en-US', {
-//     style: 'currency',
-//     currency: product.default_price.currency,
-//   }).format(product.default_price.unit_amount / 100),
-//   priceId: product.default_price.id,
-// })
-
 export const onRequestGet = async ({ env, params }) => {
   const stripe = new Stripe(env.STRIPE_API_KEY)
   const sessionId = params.id
