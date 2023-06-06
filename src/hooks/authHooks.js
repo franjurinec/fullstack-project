992d51c2973eee2c62718bde9f0157de8014e955
Fran Jurinec <franjurinec@live.com>
2023-06-07 02:37:21 +0300

    Auth Refetch Policy Fix

--- src/hooks/authHooks.js
+++ src/hooks/authHooks.js
@@ -11,6 +11,8 @@ export const useAuthStatusQuery = () =>
   useQuery({
     queryKey: [AUTH_QUERY_ID],
     queryFn: isAuthenticated,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
   })
 
 export const useAuthCreateMutation = () => {
