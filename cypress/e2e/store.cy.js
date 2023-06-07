528cb82ddf8bf0c83969aa2044f16eda4b5d37c2
Fran Jurinec <franjurinec@live.com>
2023-06-07 23:47:14 +0300

    CI Debugging

--- cypress/e2e/store.cy.js
+++ cypress/e2e/store.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="Cypress" />
 
 describe('store', () => {
+  before(() =>
+    cy.request('http://127.0.0.1:8788/hello').its('status').should('eq', 200)
+  )
+
   beforeEach(() => cy.visit('http://127.0.0.1:8788/'))
 
   it('displays store home elements (header, product cards)', () => {
