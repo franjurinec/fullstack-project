394263bf3d73243897008976eb67526d4666be0d
Fran Jurinec <franjurinec@live.com>
2023-06-07 23:56:15 +0300

    CI Debugging

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -3,7 +3,7 @@
 describe('api', () => {
   it('can be reached', () => {
     cy.request('http://127.0.0.1:8788/api/hello')
-      .its('status')
-      .should('eq', 200)
+      .its('body')
+      .should('eq', 'Hello!')
   })
 })
