5eb990b4f21f7d0e0b459b300683c21f95dbc5c7
Fran Jurinec <franjurinec@live.com>
2023-06-10 00:00:32 +0300

    Updated UI Loading and Error States

--- src/components/Store/Order/index.jsx
+++ src/components/Store/Order/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import OrderFail from './OrderFail'
 import OrderSuccess from './OrderSuccess'
 import { useCheckoutSessionQuery } from '../../../hooks/checkoutHooks'
+import { Center, Heading, Spinner } from '@chakra-ui/react'
 
 const Order = () => {
   const params = useParams()
@@ -11,7 +12,24 @@ const Order = () => {
     error,
   } = useCheckoutSessionQuery(params.id)
 
-  if (isLoading || error) return null
+  if (isLoading) {
+    return (
+      <Center>
+        <Spinner size="xl" />
+      </Center>
+    )
+  }
+
+  if (error) {
+    return (
+      <Center>
+        <Heading size="4xl" fontWeight="thin">
+          AN ERROR OCCURED
+        </Heading>
+      </Center>
+    )
+  }
+
   if (checkoutSession?.success)
     return <OrderSuccess checkoutSession={checkoutSession} />
   return <OrderFail />
